beb3940d52a1a12b9f8fffdce6a83fd6387e7f1e
Fabian Lema <fabianlemac@gmail.com>
2019-08-28 14:21:28 -0500

    DELETE and POST

--- test/bookmark.fixtures.js
+++ test/bookmark.fixtures.js
@@ -5,11 +5,25 @@ function makeBookmarksArray() {
             title: "BookMark One",
             url:'www.google.com',
             description: 'Looks like something, that looks like something. . . ',
-            rating:'5'
+            rating:5
         },
+        {
+            id: 2,
+            title: "BookMark One Two",
+            url:'www.google.com',
+            description: 'Looks like something, that looks like something. . . ',
+            rating:4
+        },
+        {
+            id: 3,
+            title: "BookMark One T",
+            url:'www.google.com.ec',
+            description: 'Looks like something, that looks like something. . . ',
+            rating:2
+        }
     ]
 };
 
-module.export = {
+module.exports = {
     makeBookmarksArray
 }
