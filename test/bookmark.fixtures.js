b01898305a9200f1f53a70db9d85c85ae6e0916a
Kiami Nichols <kiamimariah@yahoo.com>
2019-08-28 13:33:49 -0400

    need tests to be ran

---
+++ test/bookmark.fixtures.js
@@ -0,0 +1,15 @@
+function makeBookmarksArray() {
+    return [
+        {
+            id: 1,
+            title: "BookMark One",
+            url:'www.google.com',
+            description: 'Looks like something, that looks like something. . . ',
+            rating:'5'
+        },
+    ]
+};
+
+module.export = {
+    makeBookmarksArray
+}
--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,6 +1,7 @@
-const { expect } = require('chai')
-const knex = require('knex')
-const app = require('../src/app')
+const { expect } = require('chai');
+const knex = require('knex');
+const app = require('../src/app');
+const { makeBookmarkArray } = require('./bookmark.fixtures.js');
 
 describe('Bookmarks Endpoints', function() {
 let db;
@@ -18,4 +19,24 @@ before('clean the table', cleanBookmarks);
 afterEach('clean the table', cleanBookmarks);
 
 after('disconnect from db', () => db.destory());
+
+
+describe('GET /bookmark', () => {
+  context('Given bookmarks exist', () => {
+    const testbookmarks = makeArticlesArray();
+
+    beforeEach(() => {
+      return db
+        .insert(testbookmarks)
+        .into('bookmark');
+    });
+
+    it('responds 200 with bookmarks', () => {
+      return supertest(app)
+        .get('/bookmark')
+        .expect(200, testbookmarks);
+    });
+  })
 });
+});
+
